9e2d371e4812ccf35bcf0ba4ee5af05f68f17456
Hami-Hafezi <dev.HamiHafezi@gmail.com>
2025-01-21 23:40:01 +0330

    Program 3 : Area and industry from radius of a Circle that we get from user

---
+++ 3-Radius_andAreaOfCIrcle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int radius;
+    scanf("%d",&radius);
+    float area = radius*2*3.14;
+    float industry = radius*radius*3.14;
+    printf("your Circle Area is : %f\n",area);
+    printf("your Circle Area is : %f\n ", industry);
+
+    return 0;
+}
