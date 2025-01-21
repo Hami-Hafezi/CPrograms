72a51114ecbc035eabbd8594dca3de4209f5c3ba
Hami-Hafezi <dev.HamiHafezi@gmail.com>
2025-01-21 23:29:43 +0330

    Program 2 : for power 2 of a number that we get from user

---
+++ 2-NumberPower2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int numberForSetPower;
+    scanf("%d",&numberForSetPower);
+    int poweredNumber = numberForSetPower * numberForSetPower;
+    printf("%d",poweredNumber);
+    return 0;
+}
